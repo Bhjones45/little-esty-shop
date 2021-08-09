777099e50664fb72228b74f289cfd8e6bea245ac
Brett Jones <bhjones4545@gmail.com>
2021-08-09 15:22:18 -0600

    Discount edit controller methods/tests

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -15,6 +15,24 @@ class Merchant::DiscountsController < ApplicationController
     redirect_to merchant_discounts_path(merchant)
   end
 
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.discounts.find(params[:id])
+    discount.update(discount_params)
+
+    redirect_to merchant_discount_path(merchant, discount)
+  end
+
   def destroy
     merchant = Merchant.find(params[:merchant_id])
     Discount.destroy(params[:id])
