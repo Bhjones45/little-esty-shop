aec9aa85ad370c2af48c002d7fc982d2c98195d2
Brett Jones <bhjones4545@gmail.com>
2021-08-10 11:15:41 -0600

    test for highest discount

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Discount do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe 'class methods' do
+    describe 'highest discount' do
+      it 'can returns the highest available discount' do
+        merchant = create(:merchant)
+        discount1 = merchant.discounts.create(quantity_threshold: 5, percentage_discount: 10)
+        discount2 = merchant.discounts.create(quantity_threshold: 10, percentage_discount: 15)
+        discount3 = merchant.discounts.create(quantity_threshold: 15, percentage_discount: 20)
+
+        expect(Discount.highest_discount(1)).to eq(nil)
+        expect(Discount.highest_discount(7)).to eq(discount1)
+        expect(Discount.highest_discount(12)).to eq(discount2)
+        expect(Discount.highest_discount(18)).to eq(discount3)
+      end
+    end
+  end
 end
